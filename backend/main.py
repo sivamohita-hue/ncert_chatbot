02c57b320b0adc85c0976652b2865b5392817ec4
tech_stars <10428sivamohita.s@gmail.com>
2026-03-08 13:55:53 +0530

    Add files via upload

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from backend.translator import normalize_query
+from backend.search import fetch_answer
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# TEST ROUTE
+@app.get("/")
+def test():
+    return {"message": "Backend is working"}
+
+class Question(BaseModel):
+    question: str
+
+@app.post("/ask")
+async def ask_question(q: Question):
+
+    original_question = q.question
+    normalized_question = normalize_query(original_question)
+    answer = fetch_answer(normalized_question)
+
+    return {
+        "question": original_question,
+        "normalized_question": normalized_question,
+        "answer": answer
+    }
